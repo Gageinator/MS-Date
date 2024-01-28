07ff04323e80edf55c874ee9368146a5a6a2e260
RainyEmbers23 <ncompton2023@gmail.com>
2024-01-27 21:11:51 -0500

    Added introduction and Ares-Sama route
    Also added new character portraits

--- gamejamthingy/Scenes/ChoiceHandler.gd
+++ gamejamthingy/Scenes/ChoiceHandler.gd
@@ -3,11 +3,11 @@ extends Node
 const buttonprefab = preload("res://Scenes/single_choice.tscn")
 
 var choice_list = ["What do you mean?", "I don't know what you're talking about.", "Large rat"]
-var choice_count: int = len(choice_list)
 var choice_ids = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var choice_count: int = len(choice_list)
 	SignalBus.connect("dialog_chosen", buttonpress)
 	for i in range(choice_count):
 		var new_instance = buttonprefab.instantiate()
